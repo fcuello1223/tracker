1ab86633d64c010fe6e7e22e2403c3df24702729
Francisco Cuello <fcuel001@fiu.edu>
2025-02-28 12:52:54 -0500

    Set up initial server

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
+import express from 'express';
 
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Welcome to Subscription Tracker!')
+})
+
+app.listen(3003, () => {
+  console.log('API server started on port 3003');
+})
+
+export default app;
